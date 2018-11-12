d70e59e67f30b76a8b58e6b82b12935e2a966b76
Dos Santos Estrela <fabio.dossantosestrela@cgi.com>
2018-11-12 10:27:28 -0500

    unit test

--- utils-service/src/test/java/com/fabiose/utils/rest/FileRestTest.java
+++ utils-service/src/test/java/com/fabiose/utils/rest/FileRestTest.java
@@ -1,62 +1,67 @@
 package com.fabiose.utils.rest;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import com.fabiose.utils.domain.Entry; 
+
+import static org.hamcrest.CoreMatchers.equalTo; 
+import static org.junit.Assert.assertThat;
 
 public class FileRestTest {
 	
 	private FileRest fileRest;
-	private HttpPost post;
-	private HttpClient client;
 	private String url;
-	private Entry entry;
-	
+	private Entry entry;	
+	private HttpHeaders requestHeaders;
+	private HttpEntity<Entry> requestEntity; 
+	private RestTemplate restTemplate;	
+	private ResponseEntity<Entry> responseEntity;
+		
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 	
 	@Before
 	public void init() {
 		fileRest = new FileRest();
-		url = "http://localhost:8089/file";
-		post = new HttpPost(url);	
-		client = HttpClientBuilder.create().build();
 		
-	/*	entry = new Entry();
+		entry = new Entry();
+		entry.setHeader("name,project,start,end\n");
 		entry.setFile("employee.csv");
-		entry.setBody("SAM,001,2018-11-07,2019-05-07");*/
-	}
-
-	@Test
-	public void serviceNotFoundTest()throws IOException, Exception {	
-		HttpResponse response = client.execute(post);
+		entry.setBody("SAM,001,2018-11-07,2019-05-07");	
+			
+		requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(new MediaType("application","json"));
 		
-		assertThat(response.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_NOT_FOUND)); 
+		requestEntity = new HttpEntity<Entry>(entry, requestHeaders);
+		
+		restTemplate = new RestTemplate(); 
+		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());	
+	}
+ 
+	@Test
+	public void saveTest() throws IOException, Exception{
+		url = "http://localhost:8089/file/save";
+		responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Entry.class);
+		assertThat(Integer.valueOf(responseEntity.getStatusCode().toString()), equalTo(HttpStatus.SC_OK)); 
 	}
 	
 	@Test
 	public void writeIOExceptionTest() throws IOException, Exception {	
 		thrown.expect(IOException.class);
-		fileRest.write("C:\\Users\\fabio.dossantosestre\\eclipse-workspace\\filesystem", "SAM,001,2018-11-07,2019-05-07");
+		fileRest.write("C:\\Users\\fabio.dossantosestre\\eclipse-workspace\\filesystem", entry.getBody());
 	}
 	
 	@Test
@@ -64,4 +69,12 @@ public class FileRestTest {
 		thrown.expect(Exception.class);
 		fileRest.write("C:\\Users\\fabio.dossantosestre\\eclipse-workspace\\filesystem\\employee.csv", null);
 	}
+
+	@Test
+	public void deleteTest() throws IOException, Exception{
+		url = "http://localhost:8089/file/delete";
+		responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Entry.class);
+		assertThat(Integer.valueOf(responseEntity.getStatusCode().toString()), equalTo(HttpStatus.SC_OK)); 
+	}
+	
 }
